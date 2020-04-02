e1434e210a3c558878fa8a5c06c80d20a918660c
Insomnia-2408 <s1113523@student.hsleiden.nl>
2020-04-02 10:09:50 +0200

    Fixed invisible issues on light mode.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,5 +23,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -26,6 +26,7 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
+
   }
 
 }
